eb1ac783357096d32c4d966f6c4ceada7de9aa5d
Andres Miguel Espineira <amespineira@me.com>
2020-11-01 15:26:43 -0700

    changed styling in index.css

--- src/components/ResturantDisplay.js
+++ src/components/ResturantDisplay.js
@@ -4,7 +4,19 @@ function ResturantDisplay (props){
   if(props.data){
     return (
       <p>
-      Name: {props.data.name} Address: {props.data.address1} City: {props.data.city} State: {props.data.state} Zip: {props.data.zip} Telephone: {props.data.telephone} Tags: {props.data.tags} Website: {props.data.website} Genre: {props.data.genre} Hours: {props.data.hours} Attire: {props.data.attire}
+      Name: {props.data.name}
+      <ul>
+        <li key='1'>Address: {props.data.address1} </li>
+        <li key='2'> City: {props.data.city} </li>
+        <li key='3'> State: {props.data.state} </li>
+        <li key='4'> Zip: {props.data.zip} </li>
+        <li key='5'> Telephone: {props.data.telephone} </li>
+        <li key='6'> Tags: {props.data.tags} </li>
+        <li key='7'> Website: {props.data.website} </li>
+        <li key='8'> Genre: {props.data.genre} </li>
+        <li key='9'> Hours: {props.data.hours} </li>
+        <li key='10'> Attire: {props.data.attire}</li>
+      </ul>
       </p>
     )
   }
